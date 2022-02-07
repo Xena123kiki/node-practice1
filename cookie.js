240f461855b407d92b5ec2a2dc6d42bb9661d710
Xena123kiki <ckdnwls1215@snu.ac.kr>
2022-02-07 13:44:43 +0900

    cookie

---
+++ cookie.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+    console.log(req.url, req.headers.cookie)
+    res.writeHead(200, {'Set-Cookie': 'mycookie=test'})
+    res.end('<p>Hello</p>');
+}).listen(8083, ()=>{
+    console.log('8083 port is waiting for you')
+})
